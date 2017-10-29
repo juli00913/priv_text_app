9bf30d5e2f247274f836da3cca79542003b174d2
July Slavchev <jul.slavhev.tues19@gmail.com>
2017-10-29 22:45:15 +0200

    FIx

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,18 +3,18 @@ class ArticlesController < ApplicationController
 	end
 	
 	def show
-    @article = Article.find(params[:id])
+    	@article = Article.find(params[:id])
   	end
 	
 	def create
-	@article = Article.new(article_params)
- 
-	@article.save
-	redirect_to @article
+		@article = Article.new(article_params) 
+		@article.save
+		render 'articles/url_link'
+
 	end
 	
 	private
-  	def article_params
-    params.require(:article).permit(:text)
-    end
+  		def article_params
+    		params.require(:article).permit(:text)
+    	end
 end	
